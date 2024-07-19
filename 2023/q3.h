06e5bf22ecab47fe1433bcef633dfa5a76a2a393
sbottingota <saku.bottingota@gmail.com>
2024-07-20 00:39:46 +0100

    Made better q3a using c++ (TODO: optimize)

--- 2023/q3.h
+++ 2023/q3.h
@@ -1,10 +1,33 @@
-#define N_BALLS 4
+#include <vector>
+
 #define N_SPIRES 4
+#define N_BALLS 4
 
-struct State {
-    int val[N_SPIRES][N_BALLS];
-    int steps_so_far;
+using namespace std;
+
+
+class State {
+    private:
+        int movesSoFar;
+        vector<vector<int>> val;
+
+        vector<State> possibleNextMoves;
+
+        void computePossibleNextMoves(void);
+
+        State moved(int fromSpire, int toSpire);
+    
+    public:
+        static State fromString(char *str);
+
+        State(vector<vector<int>> val, int movesSoFar = 0);
+
+        vector<State> getPossibleNextMoves(void);
+
+        int getMovesSoFar(void);
+
+        vector<int> operator[](int i);
+        bool operator==(State other);
 };
-typedef struct State State;
 
-int find_min_moves(State start_state, State end_state);
+int findMinMoves(State startState, State endState);
