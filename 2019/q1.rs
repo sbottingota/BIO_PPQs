50b3cc038caec84d671b57769ed248f4be4a7941
sbottingota <saku.bottingota@gmail.com>
2024-10-29 16:20:13 +0000

    Made palindrome checking algorithm faster for 2019 Q1

--- 2019/q1.rs
+++ 2019/q1.rs
@@ -1,5 +1,14 @@
-pub fn is_palindrome(n: u64) -> bool {
-    n.to_string().chars().eq(n.to_string().chars().rev())
+pub fn is_palindrome(mut n: u64) -> bool {
+    let mut rev_n = n.to_string().chars().rev().collect::<String>().parse::<u64>().unwrap();
+
+    for _ in 0..=n.ilog10() / 2 {
+        if n % 10 != rev_n % 10 {
+            return false;
+        }
+        n /= 10;
+        rev_n /= 10;
+    }
+    true
 }
 
 pub fn next_palindrome_number(mut n: u64) -> u64 {
